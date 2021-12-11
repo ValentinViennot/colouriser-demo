2fca431c78e9c9fd393135c0a55e8af9424dbb7a
Valentin Viennot <valentinviennot@users.noreply.github.com>
2021-12-12 00:41:23 +0100

    Frontend to display restoration options (v-alpha)
    Co-authored-by: Roger Miret Gine <Roger.Miret@gmail.com>

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,29 @@
 <script>
   import { Plane } from "svelte-loading-spinners";
+
   import Slider from "@bulatdashiev/svelte-slider";
 
-  const headers = ["Original", "Colourised V1", "Colourised V2"];
-  const versions = ["origin", "v1", "v2"];
+  import { Observable } from "rxjs";
+  // import FacePicker from "./FacePicker.svelte";
+
+  const restore_backend_url = "http://localhost:8081"; // TODO remove -dev
+  // const colorize_backend_url = "/api";
+  const colorize_backend_url = "http://localhost:8080"; // TODO remove -dev
+
+  const headers = {
+    origin: "Original",
+    v1: "Colourised V1",
+    v2: "Colourised V2",
+    restored: "Restored",
+    faces: "Face picker",
+  };
+
+  const versions_bw = ["origin", "v1", "v2"];
+  const versions_restore = ["origin", "restored", "faces"];
+
+  let versions = versions_bw;
   let data = undefined;
+  // let selectedFaces;
 
   const setImage = (version, blob) => {
     let reader = new FileReader();
@@ -12,9 +31,72 @@
     reader.onload = (e) => (data[version].image = e.target.result);
   };
 
+  // data = JSON.parse(localStorage.getItem("data")); // TODO: remove -dev
+
+  const onRestoreFaces = async (hash) => {
+    data = { ...data, restored: { hash }, faces: {} };
+    // // TODO: remove -dev
+    // data["v1"].image = undefined;
+    // data["v2"].image = undefined;
+    // localStorage.setItem("data", JSON.stringify(data));
+    // return
+    versions = versions_restore;
+    new Observable((subscriber) => {
+      let poll;
+      const requestRestoredPic = async () => {
+        const response = await fetch(`${restore_backend_url}/restore/${hash}`);
+        if (response.ok) {
+          const response_data = await response.json();
+          if (!!response_data.restored) {
+            subscriber.next(response_data);
+            return subscriber.complete();
+          }
+        }
+        poll();
+      };
+      poll = () =>
+        setTimeout(async () => {
+          await requestRestoredPic();
+        }, 3000);
+      requestRestoredPic();
+    }).subscribe((response) => {
+      const base_path = `${restore_backend_url}/img/${response.hash}`;
+      data.restored.image = `${base_path}${response.restored}`;
+      data.faces.images = response.faces.map((face_url) => ({
+        id: parseInt(face_url.split("/").reverse()[0].split(".")[0]),
+        url: `${base_path}${face_url}`,
+        disabled: true,
+      }));
+    });
+  };
+
+  const onFaceSelected = (face) => {
+    face.disabled = !face.disabled;
+    data.faces.touched = true;
+    data.faces = { ...data.faces };
+  };
+
+  const onFaceRefresh = async () => {
+    let body = new FormData();
+    body.append("hash", data.restored.hash);
+    body.append(
+      "hide_faces",
+      data.faces.images
+        .filter((image) => image.disabled)
+        .map((image) => image.id)
+        .join(",")
+    );
+    fetch(`${restore_backend_url}/restore`, {
+      method: "POST",
+      body,
+    }).then(() => onRestoreFaces(data.restored.hash));
+  };
+
   const onFileSelected = async (e) => {
+    // TODO: send to restore backend async
     let image = e.target.files[0];
     data = { v1: {}, v2: {}, origin: {} };
+    versions = versions_bw;
     setImage("origin", image);
 
     let body = new FormData();
@@ -25,12 +107,14 @@
         const blob = await result.blob();
         setImage(version, blob);
         data[version].saturation = [100];
+        data[version].hash = result.headers.get("x-image-hash");
+        // TODO: original hash must be queried
       } else data[version].error = true;
     };
 
     const colorizeVersion = async (version) =>
-      fetch(`/api/colorize?version=${version}`, {
-        // fetch(`http://localhost:8080/colorize?version=${version}`, {
+      // TODO
+      fetch(`${colorize_backend_url}/colorize?version=${version}`, {
         method: "POST",
         body,
       }).then(onReceiveColoredImg(version));
@@ -64,9 +148,16 @@
       </div>
     {:else}
       <div class="row">
-        {#each headers as header}
+        {#each versions as version}
           <div class="col-4">
-            <h2>{header}</h2>
+            <!-- TODO: restored = goback -->
+            <h2>
+              {headers[version]}
+              {#if version == "faces" && !!data.faces.touched}
+                <button on:click={onFaceRefresh}>Apply</button>
+                <!-- <i class="p-icon--success"></i> -->
+              {/if}
+            </h2>
           </div>
         {/each}
       </div>
@@ -80,6 +171,22 @@
                 alt={version}
                 style="filter: saturate({data[version].saturation ?? 100}%);"
               />
+            {:else if !!data[version].images}
+              <div class="p-logo-section">
+                <div class="p-logo-section__items">
+                  {#each data[version].images as face}
+                    <div class="p-logo-section__item">
+                      <img
+                        src={face.url}
+                        alt={"face " + face.id}
+                        class={face.disabled ? "face disabled" : "face"}
+                        on:click={() => onFaceSelected(face)}
+                      />
+                    </div>
+                  {/each}
+                </div>
+              </div>
+              <!-- <FacePicker bind:selected={selectedFaces} faces={data[version].images} /> -->
             {:else if !!data[version].error}
               <p>
                 Error getting version {version}
@@ -97,6 +204,12 @@
           <div class="col-4 slider">
             {#if !!data[version].saturation}
               <Slider max="300" bind:value={data[version].saturation} />
+            {/if}
+            <!-- TODO: enable face restoration on colourised and original picture -->
+            {#if !!data[version].hash}
+              <button on:click={() => onRestoreFaces(data[version].hash)}>
+                {data[version].hash}
+              </button>
             {/if}
           </div>
         {/each}
@@ -141,6 +254,14 @@
 
   img {
     width: 100%;
+
+    &.face {
+      padding: 10px;
+    }
+
+    &.disabled {
+      opacity: 0.5;
+    }
   }
 
   .row div.slider {
---
+++ frontend/src/FacePicker.svelte
@@ -0,0 +1,47 @@
+<script>
+  export let selected = [];
+  export let faces = [];
+
+  const onFaceSelected = (face) => {
+    if (selected.includes(face.id)) {
+      selected = selected.filter((s) => s !== face.id);
+    } else {
+      selected = [...selected, face.id];
+    }
+  };
+</script>
+
+<div class="face-picker p-logo-section">
+  <div class="p-logo-section__items">
+    {#each faces as face}
+      <div class="p-logo-section__item">
+        <img
+          class="face"
+          src={face.url}
+          alt={"face " + face.id}
+          class:disabled={!selected.includes(face.id)}
+          on:click={() => onFaceSelected(face)}
+        />
+      </div>
+    {/each}
+  </div>
+</div>
+
+<style lang="scss">
+  .face-picker {
+    border: 1px solid red !important;
+    background-color: lightcoral;
+  }
+
+  img {
+    width: 100%;
+
+    &.face {
+      padding: 10px;
+    }
+
+    &.disabled {
+      opacity: 0.5;
+    }
+  }
+</style>
